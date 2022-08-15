1e1753f44f1c93509103e7d423059fede34b0c94
Creator Asert <asertcreator2000@gmail.com>
2022-08-15 14:00:40 +0900

    more things p.1

---
+++ wdOS.Plugs/DelegatePlug.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using IL2CPU.API.Attribs;
+
+namespace wdOS.Plugs
+{
+    [Plug(Target = typeof(MulticastDelegate))]
+    public static unsafe class DelegatePlug
+    {
+        public static int GetHashCode(Delegate aThis, [FieldAccess(Name = "System.IntPtr System.Delegate._methodPtr")] ref IntPtr aAddress)
+        {
+            return (int)aAddress.ToPointer();
+        }
+    }
+}
