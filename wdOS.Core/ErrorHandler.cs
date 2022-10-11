c2932a5d648cd2b89483bedc3d41744eadcaa401
Creator Asert <asertcreator2000@gmail.com>
2022-10-11 22:55:06 +0900

    version 0.2.2

---
+++ wdOS.Core/ErrorHandler.cs
@@ -0,0 +1,58 @@
+﻿using Cosmos.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static wdOS.Core.Kernel;
+
+namespace wdOS.Core
+{
+    internal static class ErrorHandler
+    {
+        internal static void Panic(uint message)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.BackgroundColor = ConsoleColor.Black;
+            Log($"!!! panic !!! {SystemSettings.PanicStrings[message]}");
+            Log($"Current kernel version: {KernelVersion}");
+            Console.WriteLine($"!!! panic !!! {SystemSettings.PanicStrings[message]}");
+            Console.WriteLine($"Current kernel version: {KernelVersion}");
+            WaitForShutdown(true, SystemSettings.CrashRestartTimeout);
+        }
+        internal static void HandleException(ref INTs.IRQContext context)
+        {
+            bool cancontinue = false;
+            string meaning = "program you just started tried to incorrectly interact with your system!\nUnfortunately, its impossible to evaluate what problem is.";
+            string reason = $"cpu exception {context.Interrupt}";
+            switch (context.Interrupt)
+            {
+                case 0x00: cancontinue = true; reason = "division by zero"; meaning = "program you just started tried to divide by zero!"; break;
+                case 0x04: cancontinue = false; reason = "stack overlow"; meaning = "program you just started used too much memory!"; break;
+                case 0x06: cancontinue = true; reason = "invalid opcode"; meaning = "program you just started is not compatible with your CPU!"; break;
+                case 0x0D: cancontinue = true; reason = "general protection fault"; meaning = "program you just started tried to access system memory!"; break;
+                case 0x10: cancontinue = true; reason = "x87 fpu exception"; meaning = "program you just started tried to incorrectly calculate numbers!"; break;
+                case 0x13: cancontinue = false; reason = "SSE fpu exception!"; meaning = "program you just started tried to incorrectly calculate numbers!"; break;
+            }
+            Console.ForegroundColor = ConsoleColor.Red;
+            Log($"!!! error !!! {reason}!");
+            Log($"at address: {context.EIP}, esp: {context.ESP}, ebp: {context.EBP}");
+            Log($"Meaning: {meaning}");
+            Console.WriteLine($"!!! error !!! {reason}!");
+            Console.WriteLine($"at address: {context.EIP}, esp: {context.ESP}, ebp: {context.EBP}");
+            Console.WriteLine($"Meaning: {meaning}");
+            if (cancontinue)
+            {
+                Console.Write("Do you want to continue? [y/n]: ");
+                if (Console.ReadKey().Key == ConsoleKey.Y) Console.WriteLine();
+                else ShutdownPC(false);
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else
+            {
+                Console.Write("You cannot continue using system!");
+                WaitForShutdown(true, SystemSettings.CrashRestartTimeout);
+            }
+        }
+    }
+}
--- wdOS.Core/Kernel.cs
+++ wdOS.Core/Kernel.cs
@@ -17,9 +17,8 @@ namespace wdOS.Core
     {
         internal const string SystemDriveLabel = "wdOSDisk";
         internal const string AssemblyName = nameof(Core);
-        internal static Debugger Debugger;
-        internal static Shell ShellToRun = new TShell();
-        internal static List<Application> Applications = new();
+        internal static Debugger KernelDebugger;
+        internal static Shell CurrentShell = new TShell();
         internal static List<KeyboardBase> Keyboards = new();
         internal static List<MouseBase> Mice = new();
         internal static StringBuilder SystemLog = new();
@@ -36,63 +35,49 @@ namespace wdOS.Core
         internal static class SystemSettings
         {
             internal static int CrashRestartTimeout = 5;
+            internal static Dictionary<uint, string> PanicStrings = new()
+            {
+                [0] = "MANUALLY_INITIATED_CRASH",
+                [1] = "INVALID_CPU_OPCODE",
+                [2] = "NO_BLOCK_DEVICES",
+                [3] = "NO_INPUT_DEVICES",
+                [4] = "GENERAL_PROTECTION_FAULT",
+                [5] = "SYSTEM_EXCEPTION",
+                [6] = "SYSTEM_SHUTDOWN",
+                [7] = "INVALID_CPUID"
+            };
         }
-        internal static Dictionary<uint, string> PanicStrings = new()
-        {
-            [0] = "MANUALLY_INITIATED_CRASH",
-            [1] = "INVALID_CPU_OPCODE",
-            [2] = "NO_BLOCK_DEVICES",
-            [3] = "NO_INPUT_DEVICES",
-            [4] = "GENERAL_PROTECTION_FAULT",
-            [5] = "SYSTEM_EXCEPTION",
-            [6] = "SYSTEM_SHUTDOWN",
-            [7] = "INVALID_CPUID"
-        };
         protected override void BeforeRun()
         {
             try
             {
-                // early initialization
-                {
-                    Debugger = mDebugger;
-                    for (int i = 0; i < 32; i++) INTs.SetIntHandler((byte)i, HandleException);
-                    Log($"wdOS is booting, running on {CPU.GetCPUBrandString()}");
-                    Log($"Current kernel version: {KernelVersion}");
-                }
-                Log("Done early initialization...");
-                Log("Starting late initialization...");
-                // late initialization
-                {
-                    Console.WriteLine("Starting components...");
-                    /*1*/
-                    var font = Sys.Graphics.Fonts.PCScreenFont.Default; // cosmetics, like new vga font
-                    /*1*/
-                    VGAScreen.SetFont(font.CreateVGAFont(), font.Height);
-                    /* 2*/
-                    DetectInputDevices(); // detect input devices, such as keyboard and mouse
-                    /*  3*/
-                    FileSystem.Initialize(); // initialize file system manager
-                    if (FileSystem.VFS.Disks.Count == 0) Panic(2); // if no disks are found, then panic
-                }
-                Log("Done late initialization...");
-            }
-            catch (Exception e) { Log($"Initialization failed! Message: {e.Message}"); Panic(5); }
-            try
-            {
+                KernelDebugger = mDebugger;
+                EarlyInitialization();
+                LateInitialization();
                 Log("Starting user stage...");
-                // shell startup
                 {
-                    Console.WriteLine($"Starting {ShellToRun.Name}...");
-                    ShellToRun.BeforeRun();
-                    while (ShellToRun.IsRunning) ShellToRun.Run();
+                    Console.WriteLine($"Starting {CurrentShell.Name}...");
+                    CurrentShell.BeforeRun();
+                    while (CurrentShell.IsRunning) CurrentShell.Run();
                 }
                 Log("Shell is exited!");
                 ShutdownPC(false);
             }
-            catch (Exception e) { Log($"Shell crashed! Message: {e.Message}"); Panic(5); }
+            catch (Exception e) { Log($"Execution crash! Message: {e.Message}"); Panic(5); }
         }
-        internal static void DetectInputDevices()
+        internal static void EarlyInitialization()
         {
+            for (int i = 0; i < 20; i++) INTs.SetIntHandler((byte)i, ErrorHandler.HandleException);
+            Log($"wdOS is booting, running on {CPU.GetCPUBrandString()}");
+            Log($"Current kernel version: {KernelVersion}");
+            Log("Done early initialization!");
+        }
+        internal static void LateInitialization()
+        {
+            Log("Starting late initialization...");
+            Console.WriteLine("Starting components...");
+            var font = Sys.Graphics.Fonts.PCScreenFont.Default;
+            VGAScreen.SetFont(font.CreateVGAFont(), font.Height);
             Keyboards = Cosmos.HAL.Global.GetKeyboardDevices().ToList();
             Mice = Cosmos.HAL.Global.GetMouseDevices().ToList();
             if (Keyboards.Count == 0)
@@ -100,7 +85,19 @@ namespace wdOS.Core
                 Console.WriteLine("Error! Your PC has no attahced keyboards. Without any keyboards you can't use system");
                 Panic(3);
             }
+            FileSystem.Initialize(); // initialize file system manager
+            if (FileSystem.VFS.Disks.Count == 0) Panic(2); // if no disks are found, then panic
+            Log("Done late initialization!");
         }
+        internal static int SweepTrash()
+        {
+            Log("Performing garbage cleaning...");
+            int result = Heap.Collect();
+            Log($"Sweeped {result} objects! Sweet!");
+            return result;
+        }
+        internal static void Log(string text) { KernelDebugger.Send($"[{AssemblyName}][{StringTime}] {text}"); _ = SystemLog.Append(text + '\n'); }
+        internal static void Panic(uint message) => ErrorHandler.Panic(message);
         internal static void WaitForShutdown(bool restart, int timeout)
         {
             Console.WriteLine($"{(restart ? "Restarting" : "Shutting down")} in {timeout}");
@@ -112,59 +109,6 @@ namespace wdOS.Core
             DateTime target = DateTime.Now.AddMilliseconds(timeout);
             while (DateTime.Now < target) { }
         }
-        internal static void Panic(uint message)
-        {
-            Console.ForegroundColor = ConsoleColor.DarkRed;
-            Console.BackgroundColor = ConsoleColor.Black;
-            Log($"!!! panic !!! {PanicStrings[message]}");
-            Log($"Current kernel version: {KernelVersion}");
-            Console.WriteLine($"!!! panic !!! {PanicStrings[message]}");
-            Console.WriteLine($"Current kernel version: {KernelVersion}");
-            WaitForShutdown(true, SystemSettings.CrashRestartTimeout);
-        }
-        internal static void HandleException(ref INTs.IRQContext context)
-        {
-            bool cancontinue = false;
-            string meaning = "program you just started tried to incorrectly interact with your system!\nUnfortunately, its impossible to evaluate what problem is.";
-            string reason = $"cpu exception {context.Interrupt}";
-            switch (context.Interrupt)
-            {
-                case 0x00: cancontinue = true; reason = "division by zero"; meaning = "program you just started tried to divide by zero!"; break;
-                case 0x04: cancontinue = false; reason = "stack overlow"; meaning = "program you just started used too much memory!"; break;
-                case 0x06: cancontinue = true; reason = "invalid opcode"; meaning = "program you just started is not compatible with your CPU!"; break;
-                case 0x0D: cancontinue = true; reason = "general protection fault"; meaning = "program you just started tried to access system memory!"; break;
-                case 0x10: cancontinue = true; reason = "x87 fpu exception"; meaning = "program you just started tried to incorrectly calculate numbers!"; break;
-                case 0x13: cancontinue = false; reason = "SSE fpu exception!"; meaning = "program you just started tried to incorrectly calculate numbers!"; break;
-            }
-            Console.ForegroundColor = ConsoleColor.Red;
-            Log($"!!! error !!! {reason}!");
-            Log($"at address: {context.EIP}, esp: {context.ESP}, ebp: {context.EBP}");
-            Log($"Meaning: {meaning}");
-            Console.WriteLine($"!!! error !!! {reason}!");
-            Console.WriteLine($"at address: {context.EIP}, esp: {context.ESP}, ebp: {context.EBP}");
-            Console.WriteLine($"Meaning: {meaning}");
-            if (cancontinue)
-            {
-                Console.Write("Do you want to continue? [y/n]: ");
-                if (Console.ReadKey().Key == ConsoleKey.Y) Console.WriteLine();
-                else ShutdownPC(false);
-                Console.ForegroundColor = ConsoleColor.White;
-            }
-            else
-            {
-                Console.Write("You cannot continue using system!");
-                WaitForShutdown(true, SystemSettings.CrashRestartTimeout);
-            }
-        }
-        internal static int SweepTrash()
-        {
-            Log("Performing garbage cleaning...");
-            int result = Heap.Collect();
-            Log($"Sweeped {result} objects! Sweet!");
-            return result;
-        }
-        internal static void Log(string text) { Debugger.Send($"[{AssemblyName}][{StringTime}] {text}"); _ = SystemLog.Append(text + '\n'); }
-        protected override void Run() { }
         internal static void ShutdownPC(bool restart)
         {
             try
@@ -177,5 +121,6 @@ namespace wdOS.Core
             }
             catch { if (!restart) { Panic(6); } else { Panic(6); } }
         }
+        protected override void Run() { }
     }
 }
