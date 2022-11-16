0e0403a3b95b47c0ad8755573f30c9bf42f74ce2
AsertCreator <asertcreator2000@gmail.com>
2022-11-16 20:13:23 +0900

    version 0.4.0 beta 2/4 but its mt

--- wdOS.Core/OS/Shells/Utilities.cs
+++ wdOS.Core/OS/Shells/Utilities.cs
@@ -1,10 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using Cosmos.Core;
+using System.Collections.Generic;
 using System.Linq;
 using wdOS.Core.OS.Foundation;
 
 namespace wdOS.Core.OS.Shells
 {
-    internal static class Utilities
+    internal static unsafe class Utilities
     {
         internal static T[] SkipArray<T>(T[] array, int count)
         {
@@ -21,6 +22,14 @@ namespace wdOS.Core.OS.Shells
             LogType.Error => "ERRO",
             _ => "UNKW",
         };
+        internal static byte[] FromStructure<T>(T struc) where T : struct
+        {
+            byte[] bytes = new byte[512];
+            byte* point = (byte*)GCImplementation.GetSafePointer(struc);
+            for (int i = 0; i < 512; i++) bytes[i] = point[i];
+            return bytes;
+        }
+        internal static 
         internal static string ConnectArgs(string[] args) => ConnectArgs(args, ' ');
         internal static string ConnectArgs(string[] args, char sep) => string.Join(sep, args);
         internal static bool HasFlag(int value, int match) => (value & match) != 0;
