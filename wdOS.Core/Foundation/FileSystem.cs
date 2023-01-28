c64852fb81a8f21fe025f3a574be6214ec761ed3
AsertCreator <asertcreator2000@gmail.com>
2023-01-28 16:21:10 +0900

    gimme 0.5.0 back now

--- wdOS.Core/Foundation/FileSystem.cs
+++ wdOS.Core/Foundation/FileSystem.cs
@@ -2,7 +2,6 @@
 using Cosmos.System.FileSystem.VFS;
 using System.Collections.Generic;
 using System.IO;
-using wdOS.Core.Foundation.FSHandlers;
 
 namespace wdOS.Core.Foundation
 {
@@ -14,13 +13,8 @@ namespace wdOS.Core.Foundation
         internal const string UserTrashes = PrivateDir + "/UserTrashes";
         internal const string SystemDir = PrivateDir + "/System";
         internal const string UserDataDir = "/Users";
-        internal static FileSystemHandler RootHandler;
         internal static void Initialize()
         {
-            if (Kernel.SystemSettings.CDROMBoot)
-            {
-                RootHandler = new CDRamdiskFileSystemHandler();
-            }
             VFSManager.RegisterVFS(VFS, false);
         }
         internal static byte[] StreamFullRead(Stream stream)
@@ -73,28 +67,7 @@ namespace wdOS.Core.Foundation
             if (DirectoryExists(filepath)) return;
             if (FileExists(filepath)) { VFSManager.DeleteFile(filepath); }
         }
-        internal static string[] GetEntryListing(string dirpath)
-        {
-            if (DirectoryExists(dirpath)) return RootHandler.GetEntryListing(dirpath);
-            else return null;
-        }
         internal static bool FileExists(string filepath) => VFSManager.FileExists(filepath);
         internal static bool DirectoryExists(string dirpath) => VFSManager.DirectoryExists(dirpath);
-    }
-    internal abstract class FileSystemHandler
-    {
-        internal abstract string Name { get; }
-        internal abstract string Desc { get; }
-        internal abstract int Version { get; }
-        internal abstract string ReadStringFile(string filepath);
-        internal abstract void WriteStringFile(string filepath, string data);
-        internal abstract byte[] ReadBytesFile(string filepath);
-        internal abstract void WriteBytesFile(string filepath, byte[] data);
-        internal abstract void CreateDirectory(string dirpath);
-        internal abstract void DeleteDirectory(string dirpath);
-        internal abstract void DeleteFile(string filepath);
-        internal abstract bool FileExists(string filepath);
-        internal abstract bool DirectoryExists(string dirpath);
-        internal abstract string[] GetEntryListing(string dirpath);
     }
 }
