36209fd3ddb6014953cddd731a4b703663674709
some guy <53435943+AsertCreator@users.noreply.github.com>
2023-06-10 11:00:58 +0900

    wdos-runtime: relocate

---
+++ platform/wdOS.Runtime/IRuntimeComponent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using wdOS.Pillow;
+
+namespace wdOS.Platform
+{
+    public interface IRuntimeComponent
+    {
+        public EEExecutable GetLibrary();
+        public string GetName();
+    }
+}
