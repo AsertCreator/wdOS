54dcba890ede0ce9e5c5bc3132a077d8b9a4127c
some guy <53435943+AsertCreator@users.noreply.github.com>
2023-06-13 16:26:52 +0900

    wdos: remove wdOS.Core reference and rewrite comment a bit

--- platform/wdOS/Kernel.cs
+++ platform/wdOS/Kernel.cs
@@ -8,7 +8,6 @@ using Sys = Cosmos.System;
 namespace wdOS
 {
     // simple bootstrapper
-    // if you want featureful and heck of a lot buggy OS, go to wdOS.Core
     // if you want somewhat featureless and almost bug-free OS, go to wdOS.Platform
     [SupportedOSPlatform("windows")]
     public class Kernel : Sys.Kernel
