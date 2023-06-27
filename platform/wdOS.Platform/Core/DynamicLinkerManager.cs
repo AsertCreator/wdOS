c271de1c8654b85dfa71650931b9997bde5d6d33
some guy <53435943+AsertCreator@users.noreply.github.com>
2023-06-27 14:00:39 +0900

    dynamic-linker-manager: start development

---
+++ platform/wdOS.Platform/Core/DynamicLinkerManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wdOS.Platform.Core
+{
+	public unsafe static class DynamicLinkerManager
+	{
+		public static T CallInUsermode<T>(void* addr)
+		{
+			var func = ((delegate* unmanaged[Cdecl]<T>)(addr));
+			return func();
+		}
+		public static T Call<T>(void* addr)
+		{
+			return ((delegate* unmanaged[Cdecl]<T>)(addr))();
+		}
+	}
+}
